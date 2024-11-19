827a417b43dd59d9186a71e906cd6627566578c7
Kang Ha Na <kanghana0124@kangnam.ac.kr>
2024-11-20 00:46:15 +0900

    Add files via upload

---
+++ keep-your-determination_K/keep-your-determination.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="작심지킴",
+    page_icon="https://cdn-icons-png.flaticon.com/128/3194/3194457.png")
+
+st.markdown(
+    """
+    ## 작심지킴
+    작심지킴은 자격증 취득 보조 웹 사이트입니다
+""")
+
+st.markdown("---")
+
+st.markdown(
+    """
+    #### 이용하는 방법?
+    1. 캘린더에 내용 저장
+    2. 챗봇에게 질문
+"""
+)
